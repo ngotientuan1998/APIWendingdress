194b9f8c584b9e75d0a94d4d03844903baa642a4
ngotientuan1998 <ngotientuan040898@gmail.com>
2024-03-22 15:20:51 +0700

    step3

--- model/modelCustomer.js
+++ model/modelCustomer.js
@@ -1,28 +1,23 @@
+const { ObjectId } = require('bson');
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
+var modelCustomer = new mongoose.Schema({
+    nameCustomer:{
         type:String,
         required:true,
     },
+   
+    numberphone:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    }
+   
 });
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Customer', modelCustomer);
