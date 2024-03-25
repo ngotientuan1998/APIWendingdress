490e103821be22d9e8582af6cedb4cd2eb3ee644
ngotientuan1998 <ngotientuan040898@gmail.com>
2024-03-25 15:27:44 +0700

    1

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ require('dotenv').config()
 const routerService = require('./Router/routerService')
 const routerJob = require('./Router/routerJob')
 const routerLogin = require('./Router/routerLogin')
-const routerThongke = require('./Router/routerThongke')
+const routerUser= require('./Router/routerUser')
 const port = process.env.PORT || 3000
 const app = express()
 app.use('/Service',routerService)
 app.use('/Job',routerJob)
 app.use('/Login',routerLogin)
-app.use('/Thongke',routerThongke)
+app.use('/User',routerThongke)
 
 app.listen(port,()=>{       
     console.log(`Server running on the port ${port}`)
