b148a7dc9284997593c366ca7f16190d79a4bff3
ngotientuan1998 <ngotientuan040898@gmail.com>
2024-04-01 13:55:45 +0700

    1

--- Router/routerLogin.js
+++ Router/routerLogin.js
@@ -8,22 +8,26 @@ const mongoose = require('mongoose');
 
 const uri = process.env.LINK;
 
-router.post('/list_user', async (req, res) => {
+router.post('/login', async (req, res) => {
     await mongoose.connect(uri);
-
+  console.log("db connect")
     let user = await userModel.findOne({email:req.body.email});
+    console.log("connect 1")
   if(user){
     if(user.password==req.body.password){
         console.log("thanh cong");
         user.password=null;
-        return res.send(user)
-
+        res.status(200)
+        res.send({msg:"Login thành công"})
     }
     else{
+      
         console.log("mật khẩu không chính xác")
+        res.status(300)
         res.send({msg:"sai mật khẩu"})
     }
   } else{
+    res.status(401)
     console.log("lỗi")
     res.send({msg:"không tồn tại user"})
   }
